a7251abdbb90cf6737d2636a5b79862e7b72608e
Garry <personarpg7501@gmail.com>
2023-11-04 23:21:11 +0900

    변수 선언, index info style(get)

--- app.js
+++ app.js
@@ -4,3 +4,35 @@ const fs = require('fs');
 const http = require('http');
 const queryString = require('querystring');
 const contentType = require('./module/contentType');
+
+let main = "./index.html"
+let sub = "./info.html"
+let style = "./style.css"
+
+
+// createServer 
+let server = http.createServer((req,res)=> {
+  //http 모둘에서 createServer 모듈 사용
+  if(req.method === 'GET' && req.url === '/'){
+
+    //index.html
+    fs.readFile(main, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        res.writeHead(200, contentType);
+        res.end(data);
+      };
+    });  
+
+    //style.css
+    fs.readFile(style, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        res.writeHead(200, contentType);
+        res.end(data);
+      };
+    }); 
+  }
+  })
