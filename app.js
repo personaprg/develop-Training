71d5758a6a9c4d5d8e5752c064496555ffe1e795
Garry <personarpg7501@gmail.com>
2023-11-04 23:34:51 +0900

    작성 완료/확인

--- app.js
+++ app.js
@@ -37,7 +37,32 @@ let server = http.createServer((req,res)=> {
 
     
 
-  } 
+  } else if (req.method=== 'POST' && req.url === "/info.html"){
+    console.log("POST 통신 완료")
+    let body ='';
+    req.on('data', (chunk) =>{
+      body += chunk.toString();
+    })
+
+    req.on('end', () => {
+      const parsedBody = queryString.parse(body);
+      const {username, password ,email} = parsedBody;
+
+      // check 
+      console.log(`form 입력으로부터 받은 데이터 확인 ->`, parsedBody);
+      console.log(`for :`, email)
+    })
+    fs.readFile(sub, 'utf8', (err, data)=> {
+      if(err) {
+        console.log(err);
+      } else {
+        
+        res.writeHead(200, contentType)
+        res.end(data)
+      }
+    })
+  }
+});
 
 let port = 1004
 server.listen(port, () => {
