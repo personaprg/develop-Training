1db84e3e2901981eff2c6ae98f35382314f534b8
Garry <personarpg7501@gmail.com>
2023-11-04 23:18:58 +0900

    모듈 불러오기

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("hi");
+// import file system 
+const fs = require('fs');
+// import hyper text
+const http = require('http');
+const queryString = require('querystring');
+const contentType = require('./module/contentType');
